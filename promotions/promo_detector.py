50004233aeed596e6220b591a141d9e2e5c381cc
Soufiane MEFTAH <s.mef2703@gmail.com>
2025-12-24 18:08:46 +0100

    Ajout configuration sites surveillés + filtrage dans la détection

--- promotions/promo_detector.py
+++ promotions/promo_detector.py
@@ -8,6 +8,7 @@ import json
 
 WISHLIST_FILE = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'wishlist', 'wishlist.json')
 
+
 SITES = [
     {
         "name": "Amazon.fr",
@@ -23,6 +24,15 @@ SITES = [
     }
 ]
 
+SITES_CONFIG_FILE = os.path.join(os.path.dirname(__file__), 'sites_config.json')
+
+def get_active_sites():
+    if os.path.exists(SITES_CONFIG_FILE):
+        with open(SITES_CONFIG_FILE, 'r', encoding='utf-8') as f:
+            config = json.load(f)
+        return [site for site in SITES if config.get(site["name"], True)]
+    return SITES
+
 def load_wishlist():
     if not os.path.exists(WISHLIST_FILE):
         return []
@@ -32,8 +42,9 @@ def load_wishlist():
 def check_promos(return_results=False):
     wishlist = load_wishlist()
     results = []
+    active_sites = get_active_sites()
     for keyword in wishlist:
-        for site in SITES:
+        for site in active_sites:
             url = site["url"].format(query=keyword.replace(' ', '+'))
             if return_results:
                 results.append({
